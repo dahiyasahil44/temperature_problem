7928a37fe9cb769c9b803157f1a17d3ec3aed616
dahiyasahil44 <85727248+dahiyasahil44@users.noreply.github.com>
2025-02-21 17:11:16 +0530

    Add files via upload

---
+++ Tracking_Daily_Temperatures.js
@@ -0,0 +1,14 @@
+// Create an array that stores the temperatures recorded over 7 days. Use indexing to access the
+//  temperature for the 4th day and update it to a new value by adding 2 degrees. After that, log 
+// the updated temperatures for all 7 days to the console.
+
+
+// temperature (in Celcius) for 7 days 
+let arr = [22,24,25,19,17,23,24];
+
+for(i=0;i<arr.length;i++){
+    if(i==3){
+        arr[i] +=2;
+    }
+    console.log(arr[i]);
+}
